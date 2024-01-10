de2516b3e104dc1eb5f57e323306b3f78384ed65
alvaromago <aalvaromago@gmail.com>
2024-01-10 10:21:32 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MpcComponent } from './components/mpc/mpc.component';
+import { MscComponent } from './components/msc/msc.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MpcComponent,
+    MscComponent
   ],
   imports: [
     BrowserModule
