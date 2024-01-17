014139d8b650a1768278939b3598102c8c0ac3f6
alvaromago <aalvaromago@gmail.com>
2024-01-17 10:34:28 +0100

    second commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { MpcComponent } from './components/mpc/mpc.component';
 import { MscComponent } from './components/msc/msc.component';
+import { C06Component } from './components/c06/c06.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MpcComponent,
-    MscComponent
+    MscComponent,
+    C06Component
   ],
   imports: [
     BrowserModule
